cc14f5beaff717190ae435a98f25a30d37effef0
Egi Adriansyah <egiadr26@gmail.com>
2023-08-14 19:00:00 +0700

    hps file

--- admin/gejala/index.php
+++ admin/gejala/index.php
@@ -2,26 +2,41 @@
 <?php include '../templates/sidebar.php'; ?>
 <?php include '../templates/topbar.php' ?>
 
+<?php require '../../functions.php'; ?>
+
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">Daftar Gejala</h1>
+    <a href="tambah.php">Tambah Data</a>
 
-</div>
-<!-- /.container-fluid -->
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <h2>Content</h2>
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
         </div>
-    </div>
-</div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Office</th>
+                            <th>Age</th>
+                            <th>Start date</th>
+                            <th>Salary</th>
+                        </tr>
+                        <tr>
+                            <td></td>
+                        </tr>
+            </div>
+            <!-- /.container-fluid -->
 
-</div>
-<!-- End of Main Content -->
+        </div>
+        <!-- End of Main Content -->
 
-<?php
-include '../templates/footer.php'
+        <?php
+        include '../templates/footer.php'
 
-?>
+        ?>
